8040c62973b1f7d248e5031451dcc64b06563ee6
wieganta <a.wiegant@googlemail.com>
2023-03-01 12:12:19 +0100

    test

---
+++ test_matrix_vector.py
@@ -0,0 +1,31 @@
+from csr_Matrix import *
+data = [1,3,2,-1,4,5]
+indices = [0,3,1,3,0,1]
+indptr = [0,2,3,4,6]
+
+tupel = (data,indices,indptr)
+matrix_B = csr_matrix(tupel)
+
+#--------------------
+A = [[1,0,0,3],[0,2,0,0],[0,0,0,-1],[4,5,0,0]]
+matrix_A = dense_to_csr(A)
+
+x = [1,1,1,1]
+y = [0,0,0,0]
+z = [4,1,3,9]
+
+x_1 = matrix_A @ x
+y_1 = matrix_A @ y
+z_1 = matrix_A @ z
+
+print(x_1)
+print(y_1)
+print(z_1)
+
+x_1 = matrix_B @ x
+y_1 = matrix_B @ y
+z_1 = matrix_B @ z
+
+print(x_1)
+print(y_1)
+print(z_1)
