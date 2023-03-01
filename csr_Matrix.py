25bced7435577e0aa37379e00fa0cb35446d1fad
wieganta <a.wiegant@googlemail.com>
2023-03-01 16:19:19 +0100

    endfassung

--- csr_Matrix.py
+++ csr_Matrix.py
@@ -1,5 +1,63 @@
 from vector import *
+def csr_tridiagonal(tripel, n):
+    a = tripel[0]
+    b = tripel[1]
+    c = tripel[2]
 
+    data = [b, c]
+    indices = [0, 1]
+    indptr = [0, 2]
+
+    indice_1 = 0
+    indice_2 = 1
+    indice_3 = 2
+
+    for i in range(n - 2):
+        data.append(a)
+        data.append(b)
+        data.append(c)
+        indices.append(indice_1)
+        indices.append(indice_2)
+        indices.append(indice_3)
+
+        indice_1 += 1
+        indice_2 += 1
+        indice_3 += 1
+
+    data.append(a)
+    data.append(b)
+    indices.append(n - 2)
+    indices.append(n - 1)
+
+    last_indptr = 3 * n - 2
+    second_last_indptr = 3 * n - 4
+    index = list(range(5, second_last_indptr, 3))
+
+    for elem in index:
+        indptr.append(elem)
+
+    indptr.append(second_last_indptr)
+    indptr.append(last_indptr)
+
+    return (data, indices, indptr)
+
+def dense_to_csr(A):
+
+    data = []
+    indices = []
+    indptr = [0]
+    iter_indptr = 0
+
+    for i in range (len(A)):
+        for j in range(len(A)):
+            if A[i][j] != 0:
+                iter_indptr += 1
+                data.append(A[i][j])
+                indices.append(j)
+
+        indptr.append(iter_indptr)
+
+    return (data,indices,indptr)
 class csr_matrix:
 
     def __init__(self, csr_tup):
@@ -11,7 +69,6 @@ class csr_matrix:
     def __matmul__(self, x: vector):
         m = len(self.indptr) - 1
         b = [0] * m
-
         for i in range(m):
             intervall = self.indptr[i:i + 2]
             for j in range(intervall[0], intervall[1]):
@@ -20,62 +77,5 @@ class csr_matrix:
         return vector(b)
 
 
-    def csr_tridiagonal(tupel,n):
-        a = tupel[1]
-        b = tupel[2]
-        c = tupel[3]
 
-        data = [b, c]
-        indices = [0, 1]
-        indptr = [0, 2]
 
-        indice_1 = 0
-        indice_2 = 1
-        indice_3 = 2
-
-        for i in range(n - 2):
-            data.append(a)
-            data.append(b)
-            data.append(c)
-            indices.append(indice_1)
-            sindices.append(indice_2)
-            sindices.append(indice_3)
-
-            indice_1 += 1
-            indice_2 += 1
-            indice_3 += 1
-
-        data.append(a)
-        data.append(b)
-        indices.append(n - 2)
-        indices.append(n - 1)
-
-        last_indptr = 3 * n - 2
-        second_last_indptr = 3 * n - 4
-        indices = list(range(5, second_last_indptr, 3))
-
-        for elem in indices:
-            indptr.append(elem)
-
-        indptr.append(second_last_indptr)
-        indptr.append(last_indptr)
-
-        return(data,indices,indptr)
-
-    def dense_to_csr(A):
-
-        data = []
-        indices = []
-        indptr = [0]
-        iter_indptr = 0
-
-        for i in range (len(A)):
-            for j in range(len(A)):
-                if A[i][j] != 0:
-                    iter_indptr += 1
-                    data.append(A[i][j])
-                    indices.append(j)
-
-            indptr.append(iter_indptr)
-
-        return (data, indices, indptr)
