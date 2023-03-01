8c0f0deeed5ff4a215a2077bc0313dc45b1648b0
wieganta <a.wiegant@googlemail.com>
2023-03-01 16:19:54 +0100

    endfassung

--- richardson_verfahren.py
+++ richardson_verfahren.py
@@ -7,23 +7,14 @@ def richardson_verfahren (A,b,x,theta,maxiter,tol):
     vec_x = vector(x)
     r_k = 0
     tol_quadrat = tol ** 2
+    r_k_liste = []
 
     for k in range(maxiter):
         r_k = (matrix_a @ vec_x) - b
+        r_k_liste.append(r_k.euklidische_norm_quadrat())
         if r_k.euklidische_norm_quadrat() < tol_quadrat:
             break
-        vec_x = vec_x - (theta * vec_x)
+        vec_x = vec_x - (theta * r_k)
 
-    return vec_x
+    return vec_x, k, r_k.euklidische_norm_quadrat(), #r_k_liste
 
-data = [1,2,2,1,1,2,2,2]
-indices = [0,3,1,2,3,4,0,2]
-indptr = [0,2,4,5,6,8]
-tupel = (data,indices,indptr)
-b = [1,2,0,1,1]
-x = [0,0,0,0,0]
-tol =10 ** (-6)
-theta = 0.5
-maxiter = 1000
-
-richardson_verfahren (tupel,b,x,theta,maxiter,tol)
