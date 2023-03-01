32d3393a6ac2a57b305c6a856b996c8dea32d76e
wieganta <a.wiegant@googlemail.com>
2023-03-01 14:18:36 +0100

    funktionen eingebaut

--- csr_Matrix.py
+++ csr_Matrix.py
@@ -20,78 +20,62 @@ class csr_matrix:
         return vector(b)
 
 
-class csr_tridiagonal:
-    def __init__(self, a, b, c, n):
-        self.data = [b, c]
-        self.indices = [0, 1]
-        self.indptr = [0, 2]
+    def csr_tridiagonal(tupel,n):
+        a = tupel[1]
+        b = tupel[2]
+        c = tupel[3]
+
+        data = [b, c]
+        indices = [0, 1]
+        indptr = [0, 2]
 
         indice_1 = 0
         indice_2 = 1
         indice_3 = 2
 
         for i in range(n - 2):
-            self.data.append(a)
-            self.data.append(b)
-            self.data.append(c)
-            self.indices.append(indice_1)
-            self.indices.append(indice_2)
-            self.indices.append(indice_3)
+            data.append(a)
+            data.append(b)
+            data.append(c)
+            indices.append(indice_1)
+            sindices.append(indice_2)
+            sindices.append(indice_3)
 
             indice_1 += 1
             indice_2 += 1
             indice_3 += 1
 
-        self.data.append(a)
-        self.data.append(b)
-        self.indices.append(n - 2)
-        self.indices.append(n - 1)
+        data.append(a)
+        data.append(b)
+        indices.append(n - 2)
+        indices.append(n - 1)
 
         last_indptr = 3 * n - 2
         second_last_indptr = 3 * n - 4
         indices = list(range(5, second_last_indptr, 3))
 
         for elem in indices:
-            self.indptr.append(elem)
+            indptr.append(elem)
 
-        self.indptr.append(second_last_indptr)
-        self.indptr.append(last_indptr)
+        indptr.append(second_last_indptr)
+        indptr.append(last_indptr)
 
-    def __matmul__(self, x: vector):
-        m = len(self.indptr) - 1
-        b = [0] * m
+        return(data,indices,indptr)
 
-        for i in range(m):
-            intervall = self.indptr[i:i + 2]
-            for j in range(intervall[0], intervall[1]):
-                b[i] = b[i] + self.data[j] * x[self.indices[j]]
+    def dense_to_csr(A):
 
-        return vector(b)
-
-
-class dense_to_csr:
-    def __init__(self, A):
-        self.data = []
-        self.indices = []
-        self.indptr = [0]
+        data = []
+        indices = []
+        indptr = [0]
         iter_indptr = 0
 
         for i in range (len(A)):
             for j in range(len(A)):
                 if A[i][j] != 0:
                     iter_indptr += 1
-                    self.data.append(A[i][j])
-                    self.indices.append(j)
+                    data.append(A[i][j])
+                    indices.append(j)
 
-            self.indptr.append(iter_indptr)
+            indptr.append(iter_indptr)
 
-    def __matmul__(self, x: vector):
-        m = len(self.indptr) - 1
-        b = [0] * m
-
-        for i in range(m):
-            intervall = self.indptr[i:i + 2]
-            for j in range(intervall[0], intervall[1]):
-                b[i] = b[i] + self.data[j] * x[self.indices[j]]
-
-        return vector(b)
+        return (data, indices, indptr)
